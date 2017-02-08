38fbeff18124ef68356836bba4138d1eb8badc15
KhaLe <kha.le@mtech.vn>
2017-02-08 12:11:20 +0700

    dfgd

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
+app.get('/webhook', function(req, res) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === 'optimus-chat-bot') {
+        console.log("Validating webhook");
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
+});
+
 app.listen(process.env.PORT || 7777);
 
 console.log('server started at localhost:7777');
